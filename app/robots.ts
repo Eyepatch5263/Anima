4553b7caa86d26f497ae0401218b448d1b2c731a
Eyepatch5263 <pratyushpragyey@gmail.com>
2026-06-29 09:04:59 +0530

    feat: added llms.txt and add feilds in robo.txt to imrove AI citations and crawling

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,18 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://anima.pratyush.works'
 
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/', 
-      disallow: ['/api/', '/_next/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/', 
+        disallow: ['/api/', '/_next/'],
+      },
+      {
+        userAgent: ['GPTBot', 'ClaudeBot', 'PerplexityBot', 'Google-Extended'],
+        allow: '/',
+        disallow: '',
+      }
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
